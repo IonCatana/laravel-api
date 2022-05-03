43a727f0e70b5eb07a75e0f16b9a4df091974292
IonCatana <catana.ion17@yahoo.it>
2022-05-03 17:45:19 +0200

    fix order desc by created_at

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
     public function index()
     {
         //
-        $data = Post::all();
+        $data = Post::orderBy('created_at', 'desc')->limit(5)->get();
         return view('admin.posts.index', compact('data'));
     }
 
